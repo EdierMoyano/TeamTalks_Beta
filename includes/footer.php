859900b394d262d8d434d7861e18ee212b11b693
Sebastian A <jsebaslozano2006@gmail.com>
2025-05-05 10:05:40 -0500

    TeamTalks_Beta

---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+<footer style="background-color: #0E4A86;" class="text-dark pt-5 pb-4">
+    <div class="container text-center text-md-start">
+        <div class="row text-center text-md-start">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="mb-4 font-weight-bold text-primary text-white">Nosotros</h5>
+                <hr class="text-white mb-4">
+                <p>
+                    <span style="color: white !important;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quisquam laborum voluptatibus minima vitae, quod quia architecto amet unde doloremque eaque autem necessitatibus, ipsum et? Laudantium, quis! Officia, labore deleniti?</span>
+                </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="mb-4 font-weight-bold text-primary text-white">Soporte</h5>
+                <hr class="text-white mb-4">
+                <p>
+                    <a href="#" class="text-white">Tu cuenta</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white">Tus ordenes</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white">Manejo de cuenta</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white">Ayuda</a>
+                </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="mb-4 font-weight-bold text-primary text-white">Has dinero</h5>
+                <hr class="text-white mb-4">
+                <p>
+                    <a href="#" class="text-white">Vende productos</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white">Anuncios</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white">Afiliate</a>
+                </p>
+                <p>
+                    <a href="#" class="text-white">Publica</a>
+                </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="mb-4 font-weight-bold text-primary text-white">Contacto</h5>
+                <hr class="text-white mb-4">
+                <p>
+                    <li class="fas fa-home me-3 text-white"></li>
+                    <span style="color: white !important;">Ibague-Tolima</span>
+                </p>
+                <p>
+                    <li class="fas fa-envelope me-3 text-white"></li>
+                    <span style="color: white !important;">Test@gmail.com</span>
+                </p>
+                <p>
+                    <li class="fas fa-phone me-3 text-white"></li>
+                    <span style="color: white !important;">+555555</span>
+                </p>
+                <p>
+                    <li class="fas fa-print me-3 text-white"></li>
+                    <span style="color: white !important;">+555555</span>
+                </p>
+            </div>
+            <hr class="text-white mb-4">
+            <div class="text-center mb-2">
+                <p>
+                    <span style="color: white !important;">Copyright, Todos los derechos reservados</span>
+                    <a href="#"><strong class="text-primary">Test</strong>
+                    </a>
+                </p>
+            </div>
+            <div class="text-center">
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white social-link"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white social-link"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white social-link"><i class="fab fa-linkedin-in"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<header>
+    <nav class="navbar navbar-expand-lg" style="background-color: #0E4A86 ;">
+        <div class="container">
+            <!-- Logo -->
+            <a class="navbar-brand" href="index.php">
+                <img src="./assets/img/logo.png" alt="Logo de la Empresa" style="height: 100px;">
+            </a>
+
+            <!-- Botón para colapsar en dispositivos pequeños -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Enlaces de navegación -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="index.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="about_we.php">Sobre Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="contactanos.php">Contáctanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn" href="login/login.php" style="background-color:white; color:#0E4A86; ">Iniciar Sesión</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
