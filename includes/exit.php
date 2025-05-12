bcf026fed9f99bbc08adb6180e58b8d3e9f16352
Sebastian A <jsebaslozano2006@gmail.com>
2025-05-12 10:54:33 -0500

    exit

---
+++ includes/exit.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+unset($_SESSION['empresa']);
+unset($_SESSION['documento']);
+unset($_SESSION['estado']);
+unset($_SESSION['rol']);
+session_destroy();
+session_write_close();
+
+echo '<script>window.location = "../index.php"</script>';
+
+?>
